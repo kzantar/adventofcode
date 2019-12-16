042cf34bff30ce7383db0053b57fd7b4f5243ba4
Andrey Khasanov <andrey.khasanov@playrix.com>
2019-12-16 23:40:37 +0800

    Move 2018 to subdir

--- 2018/12_a.py
+++ 2018/12_a.py
@@ -5,37 +5,36 @@ from bitarray import bitarray
 state_pattern = re.compile(r'initial state: (.+)')
 rule_pattern = re.compile(r'(.{5}) => (.)')
 scaler = 5
-generations = 10000
+generations = 50000000000
+
 
 class Garden:
     def __init__(self):
-        self.state = bitarray()
+        self.state = ''
         self.rules = {}
         self.start = 0
 
     def __repr__(self):
-        return ''.join('#' if b else '.' for b in self.state)
+        return self.state
 
     def load(self, filename):
         lines = open(filename).readlines()
         line = state_pattern.match(lines[0]).group(1)
-        self.state = self.scale(bitarray(line.replace('#', '1').replace('.', '0')))
+        self.state = self.scale(line)
 
         for line in lines[2:]:
             rule, goal = rule_pattern.match(line).groups()
-            rule = rule.replace('#', '1').replace('.', '0')
-            goal = goal.replace('#', '1').replace('.', '0')
-            self.rules[rule] = int(goal)
+            self.rules[rule] = goal
 
     def scale(self, state: bitarray) -> bitarray:
-        if 1 in state[:3]:
-            state = bitarray('0' * scaler) + state
+        if '#' in state[:3]:
+            state = ('.' * scaler) + state
             self.start -= scaler
 
-        if 1 in state[-3:]:
-            state.extend('0' * scaler)
+        if '#' in state[-3:]:
+            state = state +  ('.' * scaler)
 
-        first_plant = state.index('1')
+        first_plant = state.index('#')
         if first_plant > 10:     
             new_start = first_plant - scaler
             state = state[new_start:]
@@ -44,14 +43,17 @@ class Garden:
         return state
 
     def step(self):
-        state = bitarray()
-        for idx in range(self.state.length()):
+        state = ''
+        for idx in range(len(self.state)):
             sub = self.state[idx-2:idx+3]
             if len(sub) != 5:
-                state.append(0)
+                state += '.'
                 continue
 
-            state.append(self.rules.get(sub.to01(), 0))
+            if sub in self.rules:
+                state += self.rules[sub]
+            else:
+                state += '.'
 
         self.state = self.scale(state)
         # self.state = state
@@ -60,7 +62,7 @@ class Garden:
     def score(self):
         result = 0
         for i, pot in enumerate(self.state):
-            if pot:
+            if pot == '#':
                 result += i + self.start
 
         return result
@@ -68,11 +70,14 @@ class Garden:
 
 if __name__ == '__main__':
     g = Garden()
-    g.load('12_input.txt')
+    g.load('data/12_input.txt')
 
     # print(f'{0:3}: {g}')
     for iteration in range(1, generations + 1):
         g.step()
+
+        if iteration % 100000 == 0:
+            print(f'{iteration / generations:%}')
         # print(f'{iteration:3}: {g}')
 
 
