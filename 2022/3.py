15ed87e15e782aac02a89fdc8c9300fb8f55d7d7
Andrey Khasanov <a_khasanov2@wargaming.net>
2023-04-03 18:47:42 +0300

    2022 day1 in rust

---
+++ 2022/3.py
@@ -0,0 +1,16 @@
+from common import get_puzzle
+
+
+def answer_1(puzzle):
+    pass
+
+
+def answer_2(puzzle):
+    pass
+
+
+if __name__ == "__main__":
+    puzzle = get_puzzle("")
+
+    print(f"Answer 1: {answer_1(puzzle)}")
+    print(f"Answer 2: {answer_2(puzzle)}")
